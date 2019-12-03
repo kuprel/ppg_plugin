22bb026ab61559243d3cafb46555cc35d55f9d26
Brett Kuprel <brkuprel@gmail.com>
2019-12-03 09:43:38 -0800

    stable on watches

--- example/android/app/src/main/kotlin/io/kuprel/ppg_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/io/kuprel/ppg_example/MainActivity.kt
@@ -1,6 +1,7 @@
 package io.kuprel.ppg_example
 
 import android.os.Bundle
+import android.view.WindowManager
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
@@ -8,5 +9,6 @@ class MainActivity: FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         GeneratedPluginRegistrant.registerWith(this)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
     }
 }
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,13 +1,15 @@
-import 'package:ppg/ppg.dart';
-import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:math';
+import 'package:ppg/ppg.dart';
+import 'package:flutter/material.dart';
 
-const int sampleCount = 100;
-const double initialMean = 5500000;
-const double initialMax = 250000;
+const int storeCount = 1024;
+const double displayBeatCount = 2.5;
+const double peakWindowMillis = 50;
 
 void main() => runApp(HeartMonitor());
+
+double abs(double x) => x > 0 ? x : -x;
 
 class HeartMonitor extends StatelessWidget {
   @override
@@ -27,30 +29,77 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List<double> data = List<double>.filled(sampleCount, 0);
-  int index = 0;
-  double dataMean = initialMean, dataMax = initialMax;
+  int displayCount, index = 0;
+  List<double> ppg, timestamps;
+  List<bool> isPeak;
+  double ppgMean = 0, ppgMax = 1, peakTime;
+  double dataSmoothing = 0, sampleInterval = 10, hrv = 100;
+  double beatInterval = 1000, measuredBeatInterval = 1000;
   final List<StreamSubscription<dynamic>> _streamSubscriptions =
       <StreamSubscription<dynamic>>[];
 
+  int get heartRate => (60e3 / beatInterval).round();
+  int get beatCount => isPeak.fold(0, (int t, bool i) => i ? t + 1 : t);
+
   @override
   Widget build(BuildContext context) {
+    final double mH = MediaQuery.of(context).size.height / 1000;
+
     final CustomPaint graph = CustomPaint(
       painter: PulseTrace(
-        data: data,
+        data: ppg,
         index: index,
-        dataMean: dataMean,
-        dataMax: dataMax,
+        dataMean: ppgMean,
+        dataMax: ppgMax,
+        isPeak: isPeak,
+        displayCount: displayCount,
       ),
     );
+
+    final Container heartBeatText = Container(
+      alignment: Alignment.topCenter,
+      padding: EdgeInsets.only(top: 80 * mH),
+      child: RichText(
+        textAlign: TextAlign.center,
+        text: TextSpan(
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          children: <TextSpan>[
+            TextSpan(text: '$heartRate ', style: TextStyle(fontSize: 120 * mH)),
+            TextSpan(text: 'BPM\n', style: TextStyle(fontSize: 70 * mH)),
+            TextSpan(
+                text: '${hrv.round()} ', style: TextStyle(fontSize: 80 * mH)),
+            TextSpan(text: 'ms HRV', style: TextStyle(fontSize: 55 * mH)),
+          ],
+        ),
+      ),
+    );
+
+    final Container sampleRateText = Container(
+      alignment: Alignment.bottomCenter,
+      padding: EdgeInsets.only(bottom: 50 * mH),
+      child: RichText(
+        textAlign: TextAlign.center,
+        text: TextSpan(
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 80 * mH,
+          ),
+          children: <TextSpan>[
+            TextSpan(text: '${(1000 / sampleInterval).round()} '),
+            TextSpan(text: 'HZ', style: TextStyle(fontSize: 70 * mH)),
+          ],
+        ),
+      ),
+    );
+
     return Scaffold(
       backgroundColor: Colors.black,
-      // body: SizedBox.expand(child: graph),
-      body: Container(
-        child: graph,
-        width: double.infinity,
-        height: double.infinity,
-      ),
+      body: Stack(children: <Widget>[
+        SizedBox.expand(child: graph),
+        heartBeatText,
+        sampleRateText,
+      ]),
     );
   }
 
@@ -65,31 +114,94 @@ class _HomeState extends State<Home> {
   @override
   void initState() {
     super.initState();
+    displayCount = storeCount;
+    ppg = List<double>.filled(storeCount, 0);
+    timestamps = List<double>.filled(storeCount, 0);
+    isPeak = List<bool>.filled(storeCount, false);
     _streamSubscriptions.add(ppgEvents.listen(onNewData));
   }
 
-  abs(double i) => i > 0 ? i : -i;
-
   onNewData(PPGEvent e) {
-    data[index % sampleCount] = e.x;
-    index++;
+    timestamps[index % storeCount] = e.t;
 
-    if (index % 1000 == 0) {
-      print('dataMean: $dataMean');
-      print('dataMax: $dataMax');
+    final double newData = e.x[0] + e.x[1];
+    final double oldData = ppg[(index - 1) % storeCount];
+
+    if (index == 40) {
+      ppgMean = ppg.reduce((double i, double j) => i + j) / index;
+      for (int i in Iterable<int>.generate(storeCount - index)) {
+        ppg[i + index] = ppgMean;
+      }
+      ppgMax = ppg.map((double i) => abs(i - ppgMean)).reduce(max);
     }
 
-    dataMean = 0.99 * dataMean + 0.01 * e.x;
-    absDiff(double i) => dataMean - i > 0 ? dataMean - i : i - dataMean;
-    dataMax = 0.95 * dataMax + 0.05 * data.map(absDiff).reduce(max);
+    if ((index + 1) % (500 / max(sampleInterval, 1)).ceil() == 0) {
+      if (beatInterval < 300 || (hrv > 200 && beatInterval < 1500)) {
+        dataSmoothing = 0.1 + 0.9 * dataSmoothing;
+      } else if (beatInterval > 2000) {
+        dataSmoothing = 0.9 * dataSmoothing;
+      }
+    }
+
+    ppg[index % storeCount] =
+        dataSmoothing * oldData + (1 - dataSmoothing) * newData;
+
+    sampleInterval = 0.9 * sampleInterval +
+        0.1 * (e.t - timestamps[(index - 1) % storeCount]);
+
+    final int dj = (peakWindowMillis / sampleInterval).round();
+    final int j0 = index % storeCount;
+    final int j1 = (index - dj) % storeCount;
+    final int j2 = (index - 2 * dj) % storeCount;
+
+    isPeak[j1] = false;
+    if ((ppg[j0] > ppg[j1]) && (ppg[j2] > ppg[j1])) {
+      if (peakTime == null || e.t - peakTime > beatInterval / 2) {
+        isPeak[j1] = true;
+        final double newBeatInterval =
+            peakTime == null ? beatInterval : e.t - peakTime;
+        hrv = 0.99 * hrv + 0.01 * abs(newBeatInterval - measuredBeatInterval);
+        measuredBeatInterval = newBeatInterval;
+        beatInterval = 0.9 * beatInterval + 0.1 * measuredBeatInterval;
+        peakTime = e.t;
+      }
+    }
+
+    ppgMean = 0.999 * ppgMean + 0.001 * newData;
+
+    double newMax = 0;
+    for (int i in Iterable<int>.generate(min(displayCount, index))) {
+      final int j = (index + i - 1 + storeCount - displayCount) % storeCount;
+      newMax = max(newMax, abs(ppg[j] - ppgMean));
+    }
+    ppgMax = 0.95 * ppgMax + 0.05 * newMax;
+
+    if ((index + 1) % (200 / max(sampleInterval, 1)).ceil() == 0) {
+      double newDisplayCount =
+          displayBeatCount / max(1, beatCount) * min(storeCount, index);
+      newDisplayCount = min(max(50, newDisplayCount), storeCount.toDouble());
+      displayCount = (0.9 * displayCount + 0.1 * newDisplayCount).round();
+    }
+
+    index++;
+
     setState(() {});
   }
 }
 
 class PulseTrace extends CustomPainter {
-  PulseTrace({this.data, this.index, this.dataMean, this.dataMax});
-  final int index;
+  PulseTrace({
+    this.data,
+    this.index,
+    this.dataMean,
+    this.dataMax,
+    this.isPeak,
+    this.displayCount,
+  });
+
+  final int index, displayCount;
   final List<double> data;
+  final List<bool> isPeak;
   final double dataMean, dataMax;
 
   @override
@@ -97,22 +209,34 @@ class PulseTrace extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    final Paint paint = Paint()
+    final Paint pathPaint = Paint()
       ..color = Colors.red
       ..style = PaintingStyle.stroke
-      ..strokeWidth = 3;
+      ..strokeWidth = 5;
 
-    final Path path = Path()..moveTo(0, size.height / 2);
+    final Paint peakCircle = Paint()
+      ..color = Colors.white
+      ..style = PaintingStyle.fill;
 
-    for (int i in Iterable<int>.generate(sampleCount)) {
-      final double x = size.width * i / sampleCount;
-      double y = data[(index + i) % sampleCount];
-      y -= dataMean;
-      y /= dataMax;
-      y = size.height * 0.5 * (1 + 0.5 * y);
-      if (i == 0) path.moveTo(x, y);
-      if (i > 0) path.lineTo(x, y);
+    final Paint leadCircle = Paint()
+      ..color = Colors.blue
+      ..style = PaintingStyle.fill;
+
+    final Path path = Path();
+
+    double x, y;
+
+    for (int i in Iterable<int>.generate(displayCount)) {
+      x = size.width * i / displayCount * 0.8;
+      final int j = (index + i + (storeCount - displayCount)) % storeCount;
+      y = (data[j] - dataMean) / dataMax;
+      y = size.height * 0.5 * (1.2 + 0.3 * y);
+      i == 0 ? path.moveTo(x, y) : path.lineTo(x, y);
+      if (isPeak[j]) canvas.drawCircle(Offset(x, y), 6, peakCircle);
     }
-    canvas.drawPath(path, paint);
+
+    canvas
+      ..drawPath(path, pathPaint)
+      ..drawCircle(Offset(x, y), 6, leadCircle);
   }
 }
